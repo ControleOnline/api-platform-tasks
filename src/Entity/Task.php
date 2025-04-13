6c5e7842989800319545114b8659845e8d2f9297
Luiz Kim <luiz.kim@controleonline.com>
2025-04-12 21:39:33 -0300

    Update

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -1,57 +1,52 @@
 <?php
 
-namespace ControleOnline\Entity; 
-use ControleOnline\Listener\LogListener;
+namespace ControleOnline\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\Delete;
-use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Put;
-use ApiPlatform\Metadata\Get;
-use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
-use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ControleOnline\Entity\People;
+use ControleOnline\Entity\Status;
+use ControleOnline\Entity\Category;
+use ControleOnline\Entity\Order;
+use ControleOnline\Repository\TaskRepository;
+use ControleOnline\Listener\LogListener;
+use DateTime;
+use DateTimeInterface;
 
-/**
- * Task
- */
+#[ORM\Table(name: 'tasks')]
+#[ORM\EntityListeners([LogListener::class])]
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
 #[ApiResource(
-    operations: [
-        new Get(
-            security: 'is_granted(\'ROLE_CLIENT\')',
-        ),
-        new Put(
-            security: 'is_granted(\'ROLE_CLIENT\')',
-        ),
-        new Patch(
-            security: 'is_granted(\'ROLE_CLIENT\')',
-        ), 
-        new Delete(
-            security: 'is_granted(\'ROLE_CLIENT\')',
-        ),
-        new Post(
-            security: 'is_granted(\'ROLE_CLIENT\')',
-        ),
-        new GetCollection(
-            security: 'is_granted(\'ROLE_CLIENT\')',
-            uriTemplate: '/tasks',
-        )
-    ],
-    formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']],
-    security: 'is_granted(\'ROLE_CLIENT\')',
-
+    formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => 'text/csv'],
     normalizationContext: ['groups' => ['task:read']],
-    denormalizationContext: ['groups' => ['task:write']]
+    denormalizationContext: ['groups' => ['task:write']],
+    security: "is_granted('ROLE_CLIENT')",
+    operations: [
+        new GetCollection(
+            security: "is_granted('ROLE_CLIENT')",
+            uriTemplate: '/tasks'
+        ),
+        new Get(security: "is_granted('ROLE_CLIENT')"),
+        new Post(security: "is_granted('ROLE_CLIENT')"),
+        new Put(security: "is_granted('ROLE_CLIENT')"),
+        new Patch(security: "is_granted('ROLE_CLIENT')"),
+        new Delete(security: "is_granted('ROLE_CLIENT')")
+    ]
 )]
-#[ApiFilter(filterClass: OrderFilter::class, properties: ['dueDate' => 'ASC', 'alterDate' => 'DESC'])]
-#[ApiFilter(filterClass: SearchFilter::class, properties: [
+#[ApiFilter(OrderFilter::class, properties: ['dueDate' => 'ASC', 'alterDate' => 'DESC'])]
+#[ApiFilter(SearchFilter::class, properties: [
     'provider' => 'exact',
     'taskFor' => 'exact',
     'registeredBy' => 'exact',
@@ -61,332 +56,231 @@ use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
     'category' => 'exact',
     'client' => 'exact',
     'order' => 'exact',
-    'type' => 'exact',
+    'type' => 'exact'
 ])]
-#[ORM\Table(name: 'tasks')]
-#[ORM\EntityListeners([LogListener::class])]
-#[ORM\Entity(repositoryClass: \ControleOnline\Repository\TaskRepository::class)]
-
-
 class Task
 {
-    /**
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
     #[ORM\Column(type: 'integer', nullable: false)]
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $id;
-    /**
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
     #[ORM\Column(name: 'name', type: 'string', length: 50, nullable: false)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $name;
-    /**
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
     #[ORM\Column(name: 'task_type', type: 'string', length: 50, nullable: false)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $type;
-    /**
-     * @var \DateTimeInterface
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
     #[ORM\Column(name: 'due_date', type: 'datetime', nullable: true, columnDefinition: 'DATETIME')]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $dueDate;
-    /**
-     * @var \ControleOnline\Entity\People
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: People::class)]
     #[ORM\JoinColumn(name: 'registered_by_id', referencedColumnName: 'id', nullable: true)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\People::class)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $registeredBy;
-    /**
-     * @var \ControleOnline\Entity\People
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: People::class)]
     #[ORM\JoinColumn(name: 'task_for_id', referencedColumnName: 'id', nullable: false)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\People::class)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $taskFor;
-    /**
-     * @var \ControleOnline\Entity\People
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: People::class)]
     #[ORM\JoinColumn(name: 'client_id', referencedColumnName: 'id', nullable: false)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\People::class)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $client;
-    /**
-     * @var \ControleOnline\Entity\Status
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: Status::class)]
     #[ORM\JoinColumn(name: 'task_status_id', referencedColumnName: 'id', nullable: true)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\Status::class)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $taskStatus;
-    /**
-     * @var \ControleOnline\Entity\Category
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: Category::class)]
     #[ORM\JoinColumn(name: 'category_id', referencedColumnName: 'id', nullable: false)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\Category::class)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $category;
-    /**
-     * @var \ControleOnline\Entity\Category
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: Category::class)]
     #[ORM\JoinColumn(name: 'reason_id', referencedColumnName: 'id', nullable: false)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\Category::class)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $reason;
-    /**
-     * @var \ControleOnline\Entity\Category
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: Category::class)]
     #[ORM\JoinColumn(name: 'criticality_id', referencedColumnName: 'id', nullable: false)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\Category::class)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $criticality;
-    /**
-     * @var \ControleOnline\Entity\People
-     *
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: People::class)]
     #[ORM\JoinColumn(name: 'provider_id', referencedColumnName: 'id', nullable: false)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\People::class)]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $provider;
-    /**
-     * @var \ControleOnline\Entity\Order
-     *
-     * @Groups({"task:read"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: Order::class, inversedBy: 'task')]
     #[ORM\JoinColumn(name: 'order_id', referencedColumnName: 'id', nullable: true)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\Order::class, inversedBy: 'task')]
+    #[Groups(['task:read'])]
     private $order;
-    /**
-     * @var \DateTimeInterface
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
     #[ORM\Column(name: 'created_at', type: 'datetime', nullable: false, columnDefinition: 'DATETIME')]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $createdAt;
-    /**
-     * @var \DateTimeInterface
-     * @Groups({"task:write","task:read","order:read"})
-     */
+
     #[ORM\Column(name: 'alter_date', type: 'datetime', nullable: false, columnDefinition: 'DATETIME')]
+    #[Groups(['task:write', 'task:read', 'order:read'])]
     private $alterDate;
-    /**
-     * Constructor
-     */
+
     public function __construct()
     {
-        $this->createdAt = new \DateTime('now');
-        $this->alterDate = new \DateTime('now');
+        $this->createdAt = new DateTime('now');
+        $this->alterDate = new DateTime('now');
     }
-    /**
-     * Get the value of id
-     */
+
     public function getId()
     {
         return $this->id;
     }
-    /**
-     * Get the value of name
-     */
+
     public function getName()
     {
         return $this->name;
     }
-    /**
-     * Set the value of name
-     */
+
     public function setName($name): self
     {
         $this->name = $name;
         return $this;
     }
-    /**
-     * Get the value of dueDate
-     */
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setType($type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
+
     public function getDueDate()
     {
         return $this->dueDate;
     }
-    /**
-     * Set the value of dueDate
-     */
+
     public function setDueDate($dueDate): self
     {
         $this->dueDate = $dueDate;
         return $this;
     }
-    /**
-     * Get the value of registeredBy
-     */
+
     public function getRegisteredBy()
     {
         return $this->registeredBy;
     }
-    /**
-     * Set the value of registeredBy
-     */
+
     public function setRegisteredBy($registeredBy): self
     {
         $this->registeredBy = $registeredBy;
         return $this;
     }
-    /**
-     * Get the value of taskFor
-     */
+
     public function getTaskFor()
     {
         return $this->taskFor;
     }
-    /**
-     * Set the value of taskFor
-     */
+
     public function setTaskFor($taskFor): self
     {
         $this->taskFor = $taskFor;
         return $this;
     }
-    /**
-     * Get the value of taskStatus
-     */
-    public function getTaskStatus()
-    {
-        return $this->taskStatus;
-    }
-    /**
-     * Set the value of taskStatus
-     */
-    public function setTaskStatus($taskStatus): self
-    {
-        $this->taskStatus = $taskStatus;
-        return $this;
-    }
-    /**
-     * Get the value of Category
-     */
-    public function getCategory()
-    {
-        return $this->category;
-    }
-    /**
-     * Set the value of Category
-     */
-    public function setCategory($category): self
-    {
-        $this->category = $category;
-        return $this;
-    }
-    /**
-     * Get the value of reason
-     */
-    public function getReason()
-    {
-        return $this->reason;
-    }
-    /**
-     * Set the value of reason
-     */
-    public function setReason($reason): self
-    {
-        $this->reason = $reason;
-        return $this;
-    }
-    /**
-     * Get the value of criticality
-     */
-    public function getCriticality()
-    {
-        return $this->criticality;
-    }
-    /**
-     * Set the value of criticality
-     */
-    public function setCriticality($criticality): self
-    {
-        $this->criticality = $criticality;
-        return $this;
-    }
-    /**
-     * Get the value of provider
-     */
-    public function getProvider()
-    {
-        return $this->provider;
-    }
-    /**
-     * Set the value of provider
-     */
-    public function setProvider($provider): self
-    {
-        $this->provider = $provider;
-        return $this;
-    }
-    /**
-     * Get the value of client
-     */
+
     public function getClient()
     {
         return $this->client;
     }
-    /**
-     * Set the value of client
-     */
+
     public function setClient($client): self
     {
         $this->client = $client;
         return $this;
     }
-    /**
-     * Get the value of order
-     */
+
+    public function getTaskStatus()
+    {
+        return $this->taskStatus;
+    }
+
+    public function setTaskStatus($taskStatus): self
+    {
+        $this->taskStatus = $taskStatus;
+        return $this;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function setCategory($category): self
+    {
+        $this->category = $category;
+        return $this;
+    }
+
+    public function getReason()
+    {
+        return $this->reason;
+    }
+
+    public function setReason($reason): self
+    {
+        $this->reason = $reason;
+        return $this;
+    }
+
+    public function getCriticality()
+    {
+        return $this->criticality;
+    }
+
+    public function setCriticality($criticality): self
+    {
+        $this->criticality = $criticality;
+        return $this;
+    }
+
+    public function getProvider()
+    {
+        return $this->provider;
+    }
+
+    public function setProvider($provider): self
+    {
+        $this->provider = $provider;
+        return $this;
+    }
+
     public function getOrder()
     {
         return $this->order;
     }
-    /**
-     * Set the value of order
-     */
+
     public function setOrder($order): self
     {
         $this->order = $order;
         return $this;
     }
-    /**
-     * Get the value of createdAt
-     */
+
     public function getCreatedAt()
     {
         return $this->createdAt;
     }
-    /**
-     * Get the value of alterDate
-     */
+
     public function getAlterDate()
     {
         return $this->alterDate;
-    }
-    /**
-     * Get the value of type
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-    /**
-     * Set the value of type
-     */
-    public function setType($type): self
-    {
-        $this->type = $type;
-        return $this;
     }
 }
--- src/Entity/TaskInteration.php
+++ src/Entity/TaskInteration.php
@@ -1,233 +1,177 @@
 <?php
 
-namespace ControleOnline\Entity; 
-use ControleOnline\Listener\LogListener;
+namespace ControleOnline\Entity;
 
-use ApiPlatform\Metadata\Post;
-use ApiPlatform\Metadata\GetCollection;
-use ApiPlatform\Metadata\Put;
-use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\ApiProperty;
-use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
-use stdClass;
+use ControleOnline\Entity\People;
+use ControleOnline\Entity\Task;
+use ControleOnline\Entity\File;
+use ControleOnline\Repository\TaskInterationRepository;
+use ControleOnline\Listener\LogListener;
+use DateTime;
+use DateTimeInterface;
 
-/**
- * TaskInteration
- */
-#[ApiResource(
-    operations: [
-        new Get(security: 'is_granted(\'ROLE_CLIENT\')'),
-        new Post(
-            security: 'is_granted(\'ROLE_ADMIN\') or (is_granted(\'ROLE_CLIENT\'))',
-        ),
-        new GetCollection(security: 'is_granted(\'ROLE_CLIENT\')'),
-
-    ],
-    formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => ['text/csv']],
-    security: 'is_granted(\'ROLE_CLIENT\')',
-    normalizationContext: ['groups' => ['task_interaction:read']],
-    denormalizationContext: ['groups' => ['task_interaction:write']]
-)]
-#[ApiFilter(filterClass: SearchFilter::class, properties: ['task' => 'exact', 'task.id' => 'exact', 'task.taskFor' => 'exact', 'registeredBy' => 'exact', 'type' => 'exact', 'visibility' => 'exact', 'read' => 'exact'])]
 #[ORM\Table(name: 'task_interations')]
 #[ORM\EntityListeners([LogListener::class])]
-#[ORM\Entity(repositoryClass: \ControleOnline\Repository\TaskInterationRepository::class)]
+#[ORM\Entity(repositoryClass: TaskInterationRepository::class)]
+#[ApiResource(
+    formats: ['jsonld', 'json', 'html', 'jsonhal', 'csv' => 'text/csv'],
+    normalizationContext: ['groups' => ['task_interaction:read']],
+    denormalizationContext: ['groups' => ['task_interaction:write']],
+    security: "is_granted('ROLE_CLIENT')",
+    operations: [
+        new GetCollection(security: "is_granted('ROLE_CLIENT')"),
+        new Get(security: "is_granted('ROLE_CLIENT')"),
+        new Post(security: "is_granted('ROLE_ADMIN') or is_granted('ROLE_CLIENT')")
+    ]
+)]
+#[ApiFilter(SearchFilter::class, properties: [
+    'task' => 'exact',
+    'task.id' => 'exact',
+    'task.taskFor' => 'exact',
+    'registeredBy' => 'exact',
+    'type' => 'exact',
+    'visibility' => 'exact',
+    'read' => 'exact'
+])]
 class TaskInteration
 {
-    /**
-     *
-     * @Groups({"task_interaction:read"})
-     */
     #[ORM\Column(type: 'integer', nullable: false)]
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    #[Groups(['task_interaction:read'])]
     private $id;
-    /**
-     *
-     * @Groups({"task_interaction:read", "task_interaction:write"})
-     */
+
     #[ORM\Column(type: 'string', length: 50, nullable: false)]
+    #[Groups(['task_interaction:read', 'task_interaction:write'])]
     private $type;
-    /**
-     *
-     * @Groups({"task_interaction:read", "task_interaction:write"})
-     */
+
     #[ORM\Column(name: 'visibility', type: 'string', length: 50, nullable: false)]
+    #[Groups(['task_interaction:read', 'task_interaction:write'])]
     private $visibility;
-    /**
-     *
-     * @Groups({"task_interaction:read", "task_interaction:write"})
-     */
+
     #[ORM\Column(type: 'string', nullable: true)]
+    #[Groups(['task_interaction:read', 'task_interaction:write'])]
     private $body;
-    /**
-     * @var \ControleOnline\Entity\People
-     *
-     * @Groups({"task_interaction:read", "task_interaction:write"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: People::class)]
     #[ORM\JoinColumn(name: 'registered_by_id', referencedColumnName: 'id', nullable: false)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\People::class)]
+    #[Groups(['task_interaction:read', 'task_interaction:write'])]
     private $registeredBy;
-    /**
-     * @var \ControleOnline\Entity\Task
-     *
-     *
-     * @Groups({"task_interaction:read", "task_interaction:write"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: Task::class)]
     #[ORM\JoinColumn(name: 'task_id', referencedColumnName: 'id', nullable: true)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\Task::class)]
+    #[Groups(['task_interaction:read', 'task_interaction:write'])]
     private $task;
-    /**
-     * @var \ControleOnline\Entity\File
-     *
-     * @Groups({"task_interaction:read", "task_interaction:write"})
-     */
+
+    #[ORM\ManyToOne(targetEntity: File::class)]
     #[ORM\JoinColumn(name: 'file_id', referencedColumnName: 'id', nullable: true)]
-    #[ORM\ManyToOne(targetEntity: \ControleOnline\Entity\File::class)]
+    #[Groups(['task_interaction:read', 'task_interaction:write'])]
     private $file;
-    /**
-     * @var \DateTimeInterface
-     * @Groups({"task_interaction:read", "task_interaction:write"})
-     */
+
     #[ORM\Column(name: 'created_at', type: 'datetime', nullable: false, columnDefinition: 'DATETIME')]
+    #[Groups(['task_interaction:read', 'task_interaction:write'])]
     private $createdAt;
-    /**
-     * @Groups({"task_interaction:read", "task_interaction:write","task_interaction:write"})
-     */
-    #[ORM\Column(name: '`read`', type: 'integer', nullable: false)]
+
+    #[ORM\Column(name: 'read', type: 'integer', nullable: false)]
+    #[Groups(['task_interaction:read', 'task_interaction:write'])]
     private $read;
-    /**
-     * Constructor
-     */
+
     public function __construct()
     {
-        $this->createdAt = new \DateTime('now');
+        $this->createdAt = new DateTime('now');
         $this->visibility = 'private';
         $this->read = false;
     }
-    /**
-     * Get the value of id
-     */
+
     public function getId()
     {
         return $this->id;
     }
-    /**
-     * Get the value of type
-     */
+
     public function getType()
     {
         return $this->type;
     }
-    /**
-     * Set the value of type
-     */
+
     public function setType($type): self
     {
         $this->type = $type;
         return $this;
     }
-    /**
-     * Get body
-     *
-     * @return string
-     */
+
     public function getBody()
     {
         return $this->body;
     }
-    /**
-     * Set body
-     *
-     * @param string $body
-     * @return TaskInteration
-     */
+
     public function setBody($body)
     {
         $this->body = $body;
         return $this;
     }
-    /**
-     * Get the value of registeredBy
-     */
+
     public function getRegisteredBy()
     {
         return $this->registeredBy;
     }
-    /**
-     * Set the value of registeredBy
-     */
+
     public function setRegisteredBy($registeredBy): self
     {
         $this->registeredBy = $registeredBy;
         return $this;
     }
-    /**
-     * Get the value of task
-     */
+
     public function getTask()
     {
         return $this->task;
     }
-    /**
-     * Set the value of task
-     */
+
     public function setTask($task): self
     {
         $this->task = $task;
         return $this;
     }
-    /**
-     * Get the value of file
-     */
+
     public function getFile()
     {
         return $this->file;
     }
-    /**
-     * Set the value of file
-     */
+
     public function setFile($file): self
     {
         $this->file = $file;
         return $this;
     }
-    /**
-     * Get the value of createdAt
-     */
+
     public function getCreatedAt()
     {
         return $this->createdAt;
     }
-    /**
-     * Get the value of visibility
-     */
+
     public function getVisibility()
     {
         return $this->visibility;
     }
-    /**
-     * Set the value of visibility
-     */
+
     public function setVisibility($visibility): self
     {
         $this->visibility = $visibility;
         return $this;
     }
-    /**
-     * Get the value of read
-     */
+
     public function getRead()
     {
         return $this->read;
     }
-    /**
-     * Set the value of read
-     */
+
     public function setRead($read)
     {
         $this->read = $read;
